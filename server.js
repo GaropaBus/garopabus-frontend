54ae74e70e8acdfd8eb0b7e1334ab023e95e9248
João A. Pereira <joaoantpre@gmail.com>
2024-08-08 09:31:14 -0300

    simples design pattern

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
