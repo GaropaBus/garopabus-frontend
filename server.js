245f426cfc19801fbc94b93d712ea86629c71991
Natan Konig <93478667+NatanKonig@users.noreply.github.com>
2024-10-06 13:45:19 -0300

    build: change to https

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import express from "express";
+import https from "https";
+import http from "http"; // Módulo para criar servidores HTTP
+import fs from "fs";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -11,11 +14,26 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Configuração do servidor Express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/", express.static(path.join(__dirname, "/public")));
 
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
+// Verifica se o ambiente é de produção
+if (process.env.NODE_ENV === "production") {
+  // Carregar os certificados
+  const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/garopabus.uk/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/garopabus.uk/fullchain.pem')
+  };
+
+  // Criar o servidor HTTPS
+  https.createServer(options, app).listen(443, () => {
+    console.log(`Servidor rodando na porta 443 (HTTPS)`);
+  });
+} else {
+  // Para desenvolvimento, criar o servidor HTTP
+  http.createServer(app).listen(port, () => {
+    console.log(`Servidor rodando na porta ${port} (HTTP)`);
+  });
+}
