5bc5d803de8bd9ae6fbbe04fb6ccedf3d47b33ef
Natan Konig <93478667+NatanKonig@users.noreply.github.com>
2024-10-06 18:44:27 -0300

    fix: change name port to port_f

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 8080; 
+const port = process.env.PORT_F || 8080; 
 
 // Configuração do servidor Express
 app.use(express.json());
