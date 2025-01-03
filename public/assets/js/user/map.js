c4e08d6aa819ca95c6f621d930f00ae42e632dee
VictorFioravanteXavier <157313401+VictorFioravanteXavier@users.noreply.github.com>
2025-01-03 15:46:31 -0300

    ajustando o zoom maximo e minimo do mapa

--- public/assets/js/user/map.js
+++ public/assets/js/user/map.js
@@ -75,8 +75,11 @@ const map = new mapboxgl.Map({
   style: "mapbox://styles/natankonig/cm3t1dmi8002m01qsf07y0o6d",
   center: [-48.67506218376818, -28.09891989595239],
   zoom: 12,
-  maxBounds: bounds,
+  maxBounds: bounds, // Mantém os limites do mapa
+  minZoom: 9, // Nível mínimo de zoom (visão ampla)
+  maxZoom: 16, // Nível máximo de zoom (visão próxima)
 });
+
 
 // Waypoints (pontos de interesse)
 const waypoints = [
