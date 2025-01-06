c145e0e92e2632edf8fb962764f9bf7c1bc69c2f
Natan Konig <natanghizzo.xbox@gmail.com>
2025-01-06 14:32:16 -0300

    feat: added prettier, eslint for code formatting

--- public/assets/js/admin/map/paradas.js
+++ public/assets/js/admin/map/paradas.js
@@ -1,15 +1,15 @@
-import * as apiGet from '../../api/moldes_back/get.js'
+import * as apiGet from "../../api/moldes_back/get.js";
 
-let rotas = []
+let rotas = [];
 
-document.addEventListener('DOMContentLoaded', async () => {
-    rotas = await apiGet.getRotasList()
-    const select_rotas = document.getElementById('select-rotas');
-    for (const rota of rotas) {
-        const opt = document.createElement('option');
-        opt.value = rota.id
-        opt.textContent = rota.nome
-        
-        select_rotas.appendChild(opt)
-    }
-})
+document.addEventListener("DOMContentLoaded", async () => {
+  rotas = await apiGet.getRotasList();
+  const select_rotas = document.getElementById("select-rotas");
+  for (const rota of rotas) {
+    const opt = document.createElement("option");
+    opt.value = rota.id;
+    opt.textContent = rota.nome;
+
+    select_rotas.appendChild(opt);
+  }
+});
--- public/assets/js/api/moldes_back/put.js
+++ public/assets/js/api/moldes_back/put.js
@@ -1,56 +1,59 @@
-import { API_BASE_URL } from '../../config.js';
-const token = sessionStorage.getItem('token')
+import { API_BASE_URL } from "../../config.js";
+const token = sessionStorage.getItem("token");
 
 export const updateHorario = async (id, dados) => {
-    try {
-        const response = await fetch(`${API_BASE_URL}/api/horarios/${id}/`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({
-                "dia_semana": dados.dia_semana,
-                "hora_partida": dados.hora_partida,
-                "hora_chegada": dados.hora_chegada,
-                "id_rota": dados.id_rota
-            })
-        });
+  try {
+    const response = await fetch(`${API_BASE_URL}/api/horarios/${id}/`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        dia_semana: dados.dia_semana,
+        hora_partida: dados.hora_partida,
+        hora_chegada: dados.hora_chegada,
+        id_rota: dados.id_rota,
+      }),
+    });
 
-        if (!response.ok) {
-            throw new Error(`Erro: ${response.status} - ${response.statusText}`);
-        }
-    } catch (error) {
-        console.error('Erro ao buscar as rotas:', error.message);
+    if (!response.ok) {
+      throw new Error(`Erro: ${response.status} - ${response.statusText}`);
     }
-}
+  } catch (error) {
+    console.error("Erro ao buscar as rotas:", error.message);
+  }
+};
 
 export const updateRota = async (id, dados) => {
-    try {
-        const response = await fetch(`${API_BASE_URL}/api/rotas/${id}/`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({
-                "bairro_origem": dados.bairro_origem,
-                "bairro_destino": dados.bairro_destino,
-                "nome_variacao": dados.nome_variacao,
-                "tipo": dados.tipo,
-                "status": dados.status,
-                "id_rota_principal": dados.id_rota_principal,
-            })
-        });
+  try {
+    const response = await fetch(`${API_BASE_URL}/api/rotas/${id}/`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        bairro_origem: dados.bairro_origem,
+        bairro_destino: dados.bairro_destino,
+        nome_variacao: dados.nome_variacao,
+        tipo: dados.tipo,
+        status: dados.status,
+        id_rota_principal: dados.id_rota_principal,
+      }),
+    });
 
-        if (!response.ok) {
-            const errorText = await response.text(); // Captura o texto completo da resposta
-            console.error(`Erro detalhado do servidor (${response.status}):`, errorText);
-            throw new Error(`Erro: ${response.status} - ${errorText}`);
-        }
-
-        return await response.json(); // Retorna o JSON da resposta
-    } catch (error) {
-        console.error('Erro:', error.message);
+    if (!response.ok) {
+      const errorText = await response.text(); // Captura o texto completo da resposta
+      console.error(
+        `Erro detalhado do servidor (${response.status}):`,
+        errorText
+      );
+      throw new Error(`Erro: ${response.status} - ${errorText}`);
     }
-}
+
+    return await response.json(); // Retorna o JSON da resposta
+  } catch (error) {
+    console.error("Erro:", error.message);
+  }
+};
